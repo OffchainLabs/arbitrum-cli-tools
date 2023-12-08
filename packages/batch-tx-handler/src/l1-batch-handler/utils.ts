e406f49ab838d61f78f28f92effbcbc6d62585be
Jason-Wanxt <wanxutao123@gmail.com>
2023-12-08 15:20:34 +0800

    resolve request changes

--- packages/batch-tx-handler/src/l1-batch-handler/utils.ts
+++ packages/batch-tx-handler/src/l1-batch-handler/utils.ts
@@ -13,23 +13,10 @@ import { MessageDeliveredEvent } from '@arbitrum/sdk/dist/lib/abi/Bridge';
 import { EventArgs } from '@arbitrum/sdk/dist/lib/dataEntities/event';
 import { L1TransactionReceipt } from '@arbitrum/sdk/dist/lib/message/L1Transaction';
 import fetch from 'node-fetch';
+import { Base64 } from 'js-base64';
+import { BatchSegmentKindDelayedMessages, BatchSegmentKindL2Message, BatchSegmentKindL2MessageBrotli, BrotliMessageHeaderByte, DASMessageHeaderFlag, delayedMsgToBeAdded, L1MessageType_ethDeposit, L1MessageType_submitRetryableTx, L2MessageKind_Batch, L2MessageKind_SignedTx, MaxL2MessageSize, NovaDacUrl } from './constant';
 
-const MaxL2MessageSize = 256 * 1024;
-const BrotliMessageHeaderByte = 0;
-const DASMessageHeaderFlag = 0x80;
 
-const BatchSegmentKindL2Message = 0;
-const BatchSegmentKindL2MessageBrotli = 1;
-const BatchSegmentKindDelayedMessages = 2;
-
-const L1MessageType_submitRetryableTx = 9;
-const L1MessageType_ethDeposit = 12;
-// const L1MessageType_batchPostingReport = 13;
-const L2MessageKind_Batch = 3;
-const L2MessageKind_SignedTx = 4;
-const delayedMsgToBeAdded = 9;
-
-const NovaDacUrls = 'https://nova.arbitrum.io/das-servers';
 
 export type DelayedTxEvent = {
   inboxMessageEvent: EventArgs<InboxMessageDeliveredEvent>;
@@ -186,7 +173,7 @@ export const getRawData = async (sequencerTx: string): Promise<[Uint8Array, BigN
 };
 
 const processDASBatch = async (rawData: Uint8Array) => {
-  const req = await fetch(NovaDacUrls);
+  const req = await fetch(NovaDacUrl);
   const urls = (await req.text()).split('\n');
   if (urls.length === 0) {
     throw Error('No online das servers now');
@@ -199,49 +186,9 @@ const getDACData = async (url: string, rawData: Uint8Array) => {
   const dataHash = ethers.utils.hexlify(rawData.subarray(33, 65));
   const requestUrl = url + `/get-by-hash/` + dataHash.substring(2);
   const req = await fetch(requestUrl);
-
   const base64Data = await req.json();
-  const hexRawData = base64ToHex(base64Data.data);
-  return Uint8Array.from(Buffer.from(hexRawData, 'hex'));
+  return Base64.toUint8Array(base64Data.data);
 };
-
-const base64ToHex = (() => {
-  // Lookup tables
-  const values = [],
-    output = [];
-
-  // Main converter
-  return function base64ToHex(txt, sep = '') {
-    if (output.length <= 0) populateLookups();
-    const result = [];
-    let v1, v2, v3, v4;
-    for (let i = 0, len = txt.length; i < len; i += 4) {
-      // Map four chars to values.
-      v1 = values[txt.charCodeAt(i)];
-      v2 = values[txt.charCodeAt(i + 1)];
-      v3 = values[txt.charCodeAt(i + 2)];
-      v4 = values[txt.charCodeAt(i + 3)];
-      // Split and merge bits, then map and push to output.
-      result.push(
-        output[(v1 << 2) | (v2 >> 4)],
-        output[((v2 & 15) << 4) | (v3 >> 2)],
-        output[((v3 & 3) << 6) | v4],
-      );
-    }
-    // Trim result if the last values are '='.
-    if (v4 === 64) result.splice(v3 === 64 ? -2 : -1);
-    return result.join(sep);
-  };
-
-  function populateLookups() {
-    const keys = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
-    for (let i = 0; i < 256; i++) {
-      output.push(('0' + i.toString(16)).slice(-2));
-      values.push(0);
-    }
-    for (let i = 0; i < 65; i++) values[keys.charCodeAt(i)] = i;
-  }
-})();
 
 //TODO: get all startBlock tx in this batch
 export const getAllStartBlockTx = () => {};
